b3ea0924e01204d4578c79d9ea590aa0e94983fe
BryceTuppurainend <bryce.tuppurainen.au@gmail.com>
2022-09-19 12:48:36 +1000

    Page under development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
 import "./App.css";
 
 function App() {
+
+  const [developmentTime, setDevelopmentTime] = useState("");
+
+  useEffect(() => {
+    let completionDate = new Date("2022-10-20");
+    let today = new Date();
+    let remainingDays = Math.ceil((completionDate.getTime() - today.getTime()) / (1000 * 3600 * 24));
+    setDevelopmentTime(remainingDays);
+  }, []);
+
   return (
     <div className="App">
-      <p>Danis Music Studio - REACT APP PLACEHOLDER TEXT</p>
+      <p>This page is currently under development. Check back in {developmentTime} days</p>
+      <p>If you have any questions, please contact: bryce.tuppurainen.au@gmail.com</p>
     </div>
   );
 }
